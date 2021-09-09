f2bfc4895c5d9dfea9e247b414be3546f6a80911
Jesus Briceño <bricenoja@globalhitss.com>
2021-09-09 09:36:53 -0300

    feat: send emails from API

---
+++ server.js
@@ -0,0 +1,14 @@
+const Express = require('express');
+const app = Express();
+
+// Body Parser
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.json());
+
+app.use('/api/email', require('./src/routes/email'));
+
+// Server Setup
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
+});
---
+++ src/routes/email.js
@@ -0,0 +1,62 @@
+const Express = require('express');
+const router = Express.Router();
+const handlebars = require("nodemailer-express-handlebars");
+const nodemailer = require("nodemailer");
+const path = require("path");
+const utils = require('../utils/utils');
+
+require('dotenv').config({ path: './.env' });
+
+router.post('/send', async (req, res) => {
+    const { receiptName, receiptEmail, subject, template } = req.body;
+
+    if (utils.validateEmail(receiptEmail)) { // validates email
+        try {
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: process.env.account,
+                    pass: process.env.password,
+                },
+            });
+
+            const handlebarOptions = {
+                viewEngine: {
+                    partialsDir: path.resolve("./src/views/"),
+                    defaultLayout: false,
+                },
+                viewPath: path.resolve("./src/views/"),
+            };
+
+            transporter.use("compile", handlebars(handlebarOptions));
+
+            const mailOptions = {
+                from: `"${process.env.user}" <${process.env.account}>`,
+                to: receiptEmail,
+                subject: subject,
+                template: template,
+                context: {
+                    name: receiptName,
+                },
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log(`Message sent to: <${receiptEmail}> - Response: ${info.response}`);
+                res.json({
+                    "response": `Message sent ${info.response}`,
+                    "receipt": receiptEmail
+                });
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json('Server Error');
+        }
+    } else {
+        res.status(400).json('Invalid email address');
+    }
+});
+
+module.exports = router;
